db654a7794851f676ef75d862d3a244401a9f1d6
willissa2121 <you@example.com>
2019-05-28 22:18:31 -0700

    working

--- HangMan/app.js
+++ HangMan/app.js
@@ -1,61 +1,57 @@
-let input = document.getElementById("letter-guess")
-let empty = ""
-document.getElementById("answer").innerHTML = createString(word, letter, empty)
 
-//Main function ran onclick in HTML
-let runEvent = () => {
-  
-  let letter = document.getElementById("letter-guess").value
-  let answer = document.getElementById("answer")
+let library = () => {
   let word = "gorilla"
-
-  
-
-  // for (var i = 0; i < )
-}
-
-//Makes enter key Submit Button
-
-input.addEventListener("keyup", function(event){
-  if (event.keyCode === 13 || event.keycode ===32){
-    runEvent()
+  let wordA = []
+  for (var i = 0; i < word.length; i++) {
+    wordA.push(word[i])
   }
-})
+  return wordA
+}
+
+let genString = (word) => {
+  let array = []
+  for (var i = 0; i < word.length; i++) {
+    array.push(" - ")
+  }
+  return array
+}
+
+let word = library()
+let array = genString(word)
+let loseArray = []
+let losecount = 0
+let guesses = 6
 
 
-//Generates dashes same length as string
-let createString = (word, letter, empty) => {
-  
-  for (var i = 0; i < word.length; i++){
-    if (letter === word[i]){
-    empty += letter
-    }
-    else if (letter != word[i]){
-      empty += " - "
-      document.getElementById("wrong-guess").innerHTML = letter;
+document.onkeyup = function (event) {
+
+  if (event.keyCode > 64 && event.keyCode < 91) {
+
+
+    let t = 0
+    while (t < 1) {
+      for (var i = 0; i < word.length; i++) {
+        if (event.key === word[i]) {
+          array.splice(i, 1, event.key)
+        }
+        else if (event.key === array[i]) {
+
+        }
+        else if (loseArray.indexOf(event.key) === -1 && event.key !== word[i] && word.indexOf(event.key) === -1) {
+          // array.push(i,1," - ")
+          losecount++
+          loseArray.push(event.key)
+
+
+
+        }
+      }
+      document.getElementById("answer").innerHTML = array
+      console.log(loseArray)
+      document.getElementById("wrong-guess").innerHTML = loseArray
+      guesses --
+      document.getElementById("guesses-left").innerHTML = guesses
+      t++
     }
   }
-  return empty
 }
-
-
-let storeString = (empty) {
-  let goodString = document.getElementById("answer");
-  return goodString.innerHTML
-}
-
-
-
-
-// let stringIndex = (string , letter) => {
-//   let empty = ""
-
-//   for (var i = 0; i < string.length; i++){
-//     if (letter === string[i]){
-//       empty += letter
-//     }
-//     else{
-//       emtpy += "-"
-//     }
-//   }
-// }
